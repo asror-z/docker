ff129a32275dc05b9cc6fc093ee24b46b787b79f
Gitea <gitea@fake.local>
2022-08-02 11:06:45 +0500

    13333

--- App/Alpine/installs.sh
+++ App/Alpine/installs.sh
@@ -1,6 +1,8 @@
 #!/sbin/openrc-run
 
 
+# Install
+
 apk update
 
 apk add --no-cache openssl bash tini
@@ -11,3 +13,24 @@ apk add --no-cache openrc sudo
 apk add --no-cache openssh grep
 
 
+# MKDIR
+
+mkdir -p /run/openrc
+touch /run/openrc/softlevel
+
+# Configs
+
+mkdir -p /var/run/sshd
+chmod 755 /var/run/sshd
+
+ssh-keygen -A
+rc-update add sshd default
+
+echo "root:rootpath2412$" | chpasswd
+
+
+# SED
+
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -ri 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -ri 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
