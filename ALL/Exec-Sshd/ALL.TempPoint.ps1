104102fb7d41cee3781d7560a876ba97767e88ea
Gitea <gitea@fake.local>
2022-08-06 20:28:53 +0500

    13333333

--- ALL/Exec-Sshd/ALL.TempPoint.ps1
+++ ALL/Exec-Sshd/ALL.TempPoint.ps1
@@ -5,13 +5,6 @@ $apps = $(docker ps -a -q)
 "Processing: "
 Write-Output $apps
 
-for containerId in $(docker ps -q)
-do
-docker exec -it $containerId bash -c 'cd /var/www/html && git pull'
-done
-
-
-
 If (-Not $null -eq $apps)
 {
     docker exec -it $apps /usr/sbin/sshd
